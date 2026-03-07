b8aaaf8f163a0429fb963c80f579347300168d22
Jimmy Tran <tranlehaiquan@gmail.com>
2026-03-07 18:07:15 +0700

    feat: Add what A Bug game (#21)
    * feat: add what a bug game
    
    * feat: fix lint
    
    * chore: translate E to V
    
    * chore: update

---
+++ src/views/whack-a-bug/meta.ts
@@ -0,0 +1,9 @@
+import type { PageMeta } from '@/types/page'
+
+const meta: PageMeta = {
+  name: 'Đập Bug',
+  description: 'Đập bug để giải tỏa áp lực coding! Đập càng nhanh, vibe càng cao.',
+  author: 'Jimmy Trần',
+}
+
+export default meta
